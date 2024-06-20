8dfce32d9637500ff186ddbce21a210d0722aff9
DiwataTresor <dev@tresordiwata.com>
2024-06-20 10:09:29 +0100

    update du 20 juin 2024

--- app/fcts/helper.jsx
+++ app/fcts/helper.jsx
@@ -6,7 +6,8 @@ export const SITEWEB_URL="http://localhost:3002/";
 // export const BACKEND_URL="http://localhost/root/backend-rva-siteweb/";
 export const BACKEND_URL="https://api.oxerode.com/rva_siteweb/";
 
-export const oldUrl="http://projetsite.rva.cd/index2.php"
+// export const oldUrl="http://projetsite.rva.cd/index2.php"
+export const oldUrl="https://www.shazam.com/"
 
 
 // export const BACKEND_URL="http://oxerode.com/envoimail/";
